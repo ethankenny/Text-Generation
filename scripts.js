4881d41609a193d938c9ecde1c624e56fa85494b
ethankenny <ethankenny47@yahoo.com>
2017-04-21 23:58:41 -0400

    Added Color/Styles
    Added color and style customization

--- scripts.js
+++ scripts.js
@@ -6,17 +6,50 @@ function generateText() {
 
   document.getElementById("label").innerHTML = userText;
 
+  //Header Size Changer
   if (document.getElementById("header_1").checked) {
-    document.getElementById("label").style.font = "bold 2em serif";
+    document.getElementById("label").style.font = "2em serif";
   } else if (document.getElementById("header_2").checked) {
-    document.getElementById("label").style.font = "bold 1.5em serif";
+    document.getElementById("label").style.font = "1.5em serif";
   } else if (document.getElementById("header_3").checked) {
-    document.getElementById("label").style.font = "bold 1.17em serif";
+    document.getElementById("label").style.font = "1.17em serif";
   } else if (document.getElementById("header_4").checked) {
-    document.getElementById("label").innerHTML = "bold 1em serif";
+    document.getElementById("label").style.font = "1em serif";
   } else if (document.getElementById("header_5").checked) {
-    document.getElementById("label").style.font = "bold .83em serif";
+    document.getElementById("label").style.font = ".83em serif";
   } else if (document.getElementById("header_6").checked) {
-    document.getElementById("label").style.font = "bold .67em serif";
+    document.getElementById("label").style.font = ".67em serif";
+  }
+
+  //Color Changer
+  if (document.getElementById("red_check").checked) {
+    document.getElementById("label").style.color = "red";
+  } else if (document.getElementById("orange_check").checked) {
+    document.getElementById("label").style.color = "orange";
+  } else if (document.getElementById("yellow_check").checked) {
+    document.getElementById("label").style.color = "yellow";
+  } else if (document.getElementById("green_check").checked) {
+    document.getElementById("label").style.color = "green";
+  } else if (document.getElementById("blue_check").checked) {
+    document.getElementById("label").style.color = "blue";
+  } else if (document.getElementById("purple_check").checked) {
+    document.getElementById("label").style.color = "purple";
+  }
+
+  //Extra Styles Changer
+  if (document.getElementById("bold_check").checked) {
+    document.getElementById("label").style.fontWeight = "bold";
+  } if (document.getElementById("italics_check").checked) {
+    document.getElementById("label").style.fontStyle = "italic";
+  }
+
+  if (document.getElementById("underline_check").checked) {
+    if (document.getElementById("strikethrough_check").checked) {
+      document.getElementById("label").style.textDecoration = "underline line-through";
+    } else {
+      document.getElementById("label").style.textDecoration = "underline";
+    }
+  } else if (document.getElementById("strikethrough_check").checked) {
+    document.getElementById("label").style.textDecoration = "line-through";
   }
 }
